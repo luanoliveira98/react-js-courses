d5cb0c83de5d63c254918cf0a16fcc736635858b
Luan Oliveira <luanoliveira17@hotmail.com>
2025-05-02 16:45:35 -0300

    feat: create a react project

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# react-js-courses
+# React Js Courses
+
+In this repository there is all the content and studies carried out with react js.
---
+++ Rocketseat/Getting_Started/01-reactjs-fundamentals/src/App.jsx
@@ -0,0 +1,15 @@
+import Post from "./Post"
+
+function App() {
+  return (
+    <>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </>
+  )
+}
+
+export default App
