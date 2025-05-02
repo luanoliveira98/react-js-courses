a951deabb2aa58df777876ac8d7a42feb0f4f861
Luan Oliveira <luanoliveira17@hotmail.com>
2025-05-02 16:53:23 -0300

    class: props

--- Rocketseat/Getting_Started/01-reactjs-fundamentals/src/App.jsx
+++ Rocketseat/Getting_Started/01-reactjs-fundamentals/src/App.jsx
@@ -3,11 +3,14 @@ import { Post } from "./Post"
 export function App() {
   return (
     <>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="Luan Oliveira"
+        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo non nam similique quod harum placeat ad, velit in expedita neque ducimus, cupiditate eum? Distinctio error sequi similique aliquid ad mollitia."
+      />
+      <Post
+        author="Estefâne Oliveira"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ex doloribus cum, tempore, cumque incidunt, reiciendis consectetur ipsam aspernatur quam odit optio harum neque repudiandae blanditiis quas vel laborum corporis."
+      />
     </>
   )
 }
