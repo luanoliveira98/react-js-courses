39b9fc88e58d5ee3b3b4ad3f70373b19397beceb
Luan Oliveira <luanoliveira17@hotmail.com>
2025-05-06 16:31:51 -0300

    class: component sidebar

--- Rocketseat/Getting_Started/01-reactjs-fundamentals/src/App.jsx
+++ Rocketseat/Getting_Started/01-reactjs-fundamentals/src/App.jsx
@@ -1,20 +1,29 @@
 import { Post } from "./Post"
 import { Header } from "./components/Header"
 
+import styles from './App.module.css';
+
 import './global.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Luan Oliveira"
-        content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo non nam similique quod harum placeat ad, velit in expedita neque ducimus, cupiditate eum? Distinctio error sequi similique aliquid ad mollitia."
-      />
-      <Post
-        author="Estefâne Oliveira"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ex doloribus cum, tempore, cumque incidunt, reiciendis consectetur ipsam aspernatur quam odit optio harum neque repudiandae blanditiis quas vel laborum corporis."
-      />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Luan Oliveira"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo non nam similique quod harum placeat ad, velit in expedita neque ducimus, cupiditate eum? Distinctio error sequi similique aliquid ad mollitia."
+          />
+          <Post
+            author="Estefâne Oliveira"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ex doloribus cum, tempore, cumque incidunt, reiciendis consectetur ipsam aspernatur quam odit optio harum neque repudiandae blanditiis quas vel laborum corporis."
+          />
+        </main>
+      </div>
     </div >
   )
 }
