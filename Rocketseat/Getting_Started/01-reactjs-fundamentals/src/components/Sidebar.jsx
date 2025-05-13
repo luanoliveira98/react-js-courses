ba963b4da263b95599e15552c2a24a9c65e8d4ce
Luan Oliveira <luanoliveira17@hotmail.com>
2025-05-13 13:05:01 -0300

    class: avatar component

--- Rocketseat/Getting_Started/01-reactjs-fundamentals/src/components/Sidebar.jsx
+++ Rocketseat/Getting_Started/01-reactjs-fundamentals/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import { PencilLine } from 'phosphor-react';
 
 import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
 
 export function Sidebar() {
   return (
@@ -11,11 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/luanoliveira98.png"
-          alt=""
-        />
+        <Avatar src="https://github.com/luanoliveira98.png" />
         <strong>Luan Oliveira</strong>
         <span>Web Developer</span>
       </div>
