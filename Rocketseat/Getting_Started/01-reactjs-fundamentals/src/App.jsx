e5339bb90946c65322f55f58474c3734cf130427
Luan Oliveira <luanoliveira17@hotmail.com>
2025-05-14 11:37:51 -0300

    class: iterating in jsx

--- Rocketseat/Getting_Started/01-reactjs-fundamentals/src/App.jsx
+++ Rocketseat/Getting_Started/01-reactjs-fundamentals/src/App.jsx
@@ -6,6 +6,37 @@ import styles from './App.module.css';
 
 import './global.css';
 
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/luanoliveira98.png',
+      name: 'Luan Oliveira',
+      role: 'Web Developer',
+    },
+    content: [
+      { type: 'paragraph', content: "Hey guys👋" },
+      { type: 'paragraph', content: "I just uploaded another project to my portfolio. It's a project I did at NLW Return, a Rocketseat event. The name of the project is DoctorCare🚀" },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-05-13 11:30:00'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CEO @Rocketseat',
+    },
+    content: [
+      { type: 'paragraph', content: "Hey guys👋" },
+      { type: 'paragraph', content: "I just uploaded another project to my portfolio. It's a project I did at NLW Return, a Rocketseat event. The name of the project is DoctorCare🚀" },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2025-05-14 10:00:00'),
+  }
+]
+
 export function App() {
   return (
     <div>
@@ -14,14 +45,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post
-            author="Luan Oliveira"
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quo non nam similique quod harum placeat ad, velit in expedita neque ducimus, cupiditate eum? Distinctio error sequi similique aliquid ad mollitia."
-          />
-          <Post
-            author="Estefâne Oliveira"
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda ex doloribus cum, tempore, cumque incidunt, reiciendis consectetur ipsam aspernatur quam odit optio harum neque repudiandae blanditiis quas vel laborum corporis."
-          />
+          {posts.map(post => {
+            return (
+              <Post
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div >
