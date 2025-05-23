b928e07bb02aa3de13f7b1f75e39d9c92e26f64e
Luan Oliveira <luanoliveira17@hotmail.com>
2025-05-23 18:22:17 -0300

    class: improving inputs

--- Rocketseat/Getting_Started/02-ignite-timer/src/pages/Home/index.tsx
+++ Rocketseat/Getting_Started/02-ignite-timer/src/pages/Home/index.tsx
@@ -8,15 +8,25 @@ export function Home() {
                 <FormContainer>
                     <label htmlFor="task">I will work on</label>
                     <TaskInput 
-                        id="task" 
+                        id="task"
+                        list="task-suggestions"
                         placeholder="Name your project"
                     />
+
+                    <datalist id="task-suggestions">
+                        <option value="Project-1" />
+                        <option value="Project-2" />
+                        <option value="Project-3" />
+                    </datalist>
 
                     <label htmlFor="minutesAmount">during</label>
                     <MinutesAmountInput 
                         type="number" 
                         id="minutesAmount" 
                         placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
                     />
 
                     <span>minutes</span>
