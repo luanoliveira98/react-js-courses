d8f9f445d5c4fa09be4b77e7f9189062a59b8624
Luan Oliveira <luanoliveira17@hotmail.com>
2025-05-27 18:15:33 -0300

    class: changing page title

--- Rocketseat/Getting_Started/02-ignite-timer/src/pages/Home/index.tsx
+++ Rocketseat/Getting_Started/02-ignite-timer/src/pages/Home/index.tsx
@@ -39,10 +39,16 @@ export function Home() {
     const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
     useEffect(() => {
+        let interval: number;
+
         if(activeCycle) {
-            setInterval(() => {
+            interval = setInterval(() => {
                 setAmountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate))
             }, 1000)
+        }
+
+        return () => {
+            clearInterval(interval)
         }
     }, [activeCycle])
 
@@ -58,6 +64,7 @@ export function Home() {
 
         setCycles((state) => [...state, newCycle])
         setActiveCycleId(id)
+        setAmountSecondsPassed(0)
 
         reset()
     }
@@ -70,6 +77,12 @@ export function Home() {
 
     const minutes = String(minutesAmounts).padStart(2, '0')
     const seconds = String(secondsAmounts).padStart(2, '0')
+
+    useEffect(() => {
+        if (activeCycle) {
+            document.title = `${minutes}:${seconds}`
+        }
+    }, [minutes, seconds, activeCycle])
 
     const task = watch('task')
     const isSubmitDisabled = !task
