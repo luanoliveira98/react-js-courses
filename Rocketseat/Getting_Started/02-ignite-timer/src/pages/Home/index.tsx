9659782caba2c8b15dcfad19b26915afe9d0b99e
Luan Oliveira <luanoliveira17@hotmail.com>
2025-05-27 16:26:50 -0300

    class: react hook form

--- Rocketseat/Getting_Started/02-ignite-timer/src/pages/Home/index.tsx
+++ Rocketseat/Getting_Started/02-ignite-timer/src/pages/Home/index.tsx
@@ -1,16 +1,27 @@
 import { Play } from "phosphor-react";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButtomContainer, TaskInput } from "./styles";
+import { useForm } from "react-hook-form";
 
 export function Home() {
+    const { register, handleSubmit, watch } = useForm()
+
+    function handleCreateNewCycle(data: any) {
+
+    }
+
+    const task = watch('task')
+    const isSubmitDisabled = !task
+
     return (
         <HomeContainer>
-            <form action="">
+            <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
                 <FormContainer>
                     <label htmlFor="task">I will work on</label>
                     <TaskInput 
                         id="task"
                         list="task-suggestions"
                         placeholder="Name your project"
+                        {...register('task')}
                     />
 
                     <datalist id="task-suggestions">
@@ -27,6 +38,7 @@ export function Home() {
                         step={5}
                         min={5}
                         max={60}
+                        {...register('minutesAmount', { valueAsNumber: true })}
                     />
 
                     <span>minutes</span>
@@ -40,7 +52,7 @@ export function Home() {
                     <span>0</span>
                 </CountdownContainer>
 
-                <StartCountdownButtomContainer disabled type="submit">
+                <StartCountdownButtomContainer disabled={isSubmitDisabled} type="submit">
                     <Play size={24}/>
                     Start
                 </StartCountdownButtomContainer>
