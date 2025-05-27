b63fd247e42ed8c83e490d4025281a519b4cd5f4
Luan Oliveira <luanoliveira17@hotmail.com>
2025-05-27 17:04:33 -0300

    class: starting new cycle

--- Rocketseat/Getting_Started/02-ignite-timer/src/pages/Home/index.tsx
+++ Rocketseat/Getting_Started/02-ignite-timer/src/pages/Home/index.tsx
@@ -1,8 +1,9 @@
-import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButtomContainer, TaskInput } from "./styles";
-import { useForm } from "react-hook-form";
-import { zodResolver } from '@hookform/resolvers/zod';
-import * as zod from 'zod';
+import { Play } from "phosphor-react"
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButtomContainer, TaskInput } from "./styles"
+import { useForm } from "react-hook-form"
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+import { useState } from "react"
 
 const newCycleFormValidationSchema = zod.object({
     task: zod.string().min(1, 'Report the task'),
@@ -10,26 +11,49 @@ const newCycleFormValidationSchema = zod.object({
         .number()
         .min(5, 'The cycle must be a minimun of 60 minutes.')
         .max(60, 'The cycle must be a maximun of 60 minutes.'),
-});
+})
 
-type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+interface Cycle {
+    id: string
+    task: string
+    minutesAmount: number
+}
 
 export function Home() {
+    const [cycles, setCycles] = useState<Cycle[]>([])
+    const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+
     const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
         defaultValues: {
             task: '',
             minutesAmount: 0,
         }
-    });
+    })
 
     function handleCreateNewCycle(data: NewCycleFormData) {
-        console.log(data);
-        reset();
+        const id = String(new Date().getTime())
+
+        const newCycle: Cycle = {
+            id,
+            task: data.task,
+            minutesAmount: data.minutesAmount,
+        }
+
+        setCycles((state) => [...state, newCycle])
+        setActiveCycleId(id)
+
+        reset()
     }
 
-    const task = watch('task');
-    const isSubmitDisabled = !task;
+    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+    console.log(activeCycle)
+
+    const task = watch('task')
+    const isSubmitDisabled = !task
 
     return (
         <HomeContainer>
