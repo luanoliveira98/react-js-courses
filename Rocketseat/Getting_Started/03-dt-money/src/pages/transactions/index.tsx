da1abe5f7b311eefd7fcdae3fb7d1133a29b3806
Luan <luanoliveira17@hotmail.com>
2025-07-25 13:15:06 -0300

    class: transaction table

--- Rocketseat/Getting_Started/03-dt-money/src/pages/transactions/index.tsx
+++ Rocketseat/Getting_Started/03-dt-money/src/pages/transactions/index.tsx
@@ -1,5 +1,10 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
@@ -7,6 +12,29 @@ export function Transactions() {
       <Header />
 
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighLight variant="income">R$ 12.000,00</PriceHighLight>
+              </td>
+              <td>Venda</td>
+              <td>25/07/2025</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighLight variant="outcome">- R$ 59,00</PriceHighLight>
+              </td>
+              <td>Alimentação</td>
+              <td>24/07/2025</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   );
 }
