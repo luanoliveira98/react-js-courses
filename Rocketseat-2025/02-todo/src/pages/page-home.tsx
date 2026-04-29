091100bd448aa6fbd48b8c5a4262569221baf69a
Luan Oliveira Saldanha <luanoliveira17@hotmail.com>
2026-04-28 22:14:31 -0300

    feat: add core-components

--- Rocketseat-2025/02-todo/src/pages/page-home.tsx
+++ Rocketseat-2025/02-todo/src/pages/page-home.tsx
@@ -1,5 +1,12 @@
 import Container from "../components/container";
+import TasksSummary from "../core-components/tasks-summary";
 
 export default function PageHome() {
-  return <Container>PAGE HOME!</Container>;
+  return (
+    <Container as="article" className="space-y-3">
+      <header className="flex items-center justify-between">
+        <TasksSummary />
+      </header>
+    </Container>
+  );
 }
