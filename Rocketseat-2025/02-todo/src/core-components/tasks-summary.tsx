3acfe65fe81b2a8cbb1dadea3b8f9c215cd4197f
Luan Oliveira Saldanha <luanoliveira17@hotmail.com>
2026-04-30 20:14:22 -0300

    feat: tasks counts on badges

--- Rocketseat-2025/02-todo/src/core-components/tasks-summary.tsx
+++ Rocketseat-2025/02-todo/src/core-components/tasks-summary.tsx
@@ -1,20 +1,25 @@
 import Badge from "../components/badge";
 import Text from "../components/text";
+import useTasks from "../hooks/use-tasks";
 
 export default function TasksSummary() {
+  const { createdTasksCount, concludedTasksCount } = useTasks();
+
   return (
     <>
       <div className="flex items-center gap-2">
         <Text variant="body-sm-bold" className="text-gray-300!">
           Created tasks
         </Text>
-        <Badge variant="secondary">5</Badge>
+        <Badge variant="secondary">{createdTasksCount}</Badge>
       </div>
       <div className="flex items-center gap-2">
         <Text variant="body-sm-bold" className="text-gray-300!">
           Completed
         </Text>
-        <Badge variant="primary">2 of 5</Badge>
+        <Badge variant="primary">
+          {concludedTasksCount} of {createdTasksCount}
+        </Badge>
       </div>
     </>
   );
