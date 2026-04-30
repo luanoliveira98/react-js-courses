762db11e88fbf88f5896d861e568adc2eb6e13fa
Luan Oliveira Saldanha <luanoliveira17@hotmail.com>
2026-04-29 23:27:55 -0300

    feat: list tasks

--- Rocketseat-2025/02-todo/src/core-components/task-item.tsx
+++ Rocketseat-2025/02-todo/src/core-components/task-item.tsx
@@ -1,16 +1,25 @@
+import React from "react";
 import ButtonIcon from "../components/button-icon";
 import Card from "../components/card";
 import InputCheckbox from "../components/input-checkbox";
 import Text from "../components/text";
+
 import TrashIcon from "../assets/icons/trash.svg?react";
 import PencilIcon from "../assets/icons/pencil.svg?react";
-import { useState } from "react";
 import XIcon from "../assets/icons/x.svg?react";
 import CheckIcon from "../assets/icons/check.svg?react";
 import InputText from "../components/input-text";
+import { type Task, TaskState } from "../models/task";
+import { cx } from "class-variance-authority";
 
-export default function TaskItem() {
-  const [isEditing, setIsEditing] = useState(false);
+interface TaskItemProps {
+  task: Task;
+}
+
+export default function TaskItem({ task }: TaskItemProps) {
+  const [isEditing, setIsEditing] = React.useState(
+    task?.state === TaskState.Creating,
+  );
 
   function handleEditTask() {
     setIsEditing(true);
@@ -24,8 +33,13 @@ export default function TaskItem() {
     <Card size="md" className="flex items-center gap-4">
       {!isEditing ? (
         <>
-          <InputCheckbox />
-          <Text className="flex-1">🛒 Weekly shopping</Text>
+          <InputCheckbox
+            value={task.concluded?.toString()}
+            checked={task.concluded}
+          />
+          <Text className={cx("flex-1", { "line-through": task?.concluded })}>
+            {task?.title}
+          </Text>
           <div className="flex gap-1">
             <ButtonIcon icon={TrashIcon} variant="tertiary" />
             <ButtonIcon
