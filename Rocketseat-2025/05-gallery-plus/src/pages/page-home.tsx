bbdb67a05227c01d23fdc467d4e0942cae6a8aee
Luan Oliveira Saldanha <luanoliveira17@hotmail.com>
2026-06-03 20:40:03 -0300

    feat: add AlbumsFilter component

--- Rocketseat-2025/05-gallery-plus/src/pages/page-home.tsx
+++ Rocketseat-2025/05-gallery-plus/src/pages/page-home.tsx
@@ -1,9 +1,19 @@
 import Container from "../components/container";
+import AlbumsFilter from "../contexts/albums/components/albums-filter";
 import PhotosList from "../contexts/photos/components/photos-list";
 
 export default function PageHome() {
   return (
     <Container>
+      <AlbumsFilter
+        albums={[
+          { id: "1", title: "Sample Album" },
+          { id: "2", title: "Sample Album 2" },
+          { id: "3", title: "Sample Album 3" },
+          { id: "4", title: "Sample Album 4" },
+        ]}
+        className="mb-9"
+      />
       <PhotosList
         photos={[
           {
