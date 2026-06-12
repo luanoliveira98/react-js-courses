762ca566c0b952f1aeca5ea114789313770bdf31
Luan Oliveira Saldanha <luanoliveira17@hotmail.com>
2026-06-12 20:21:59 -0300

    refactor: gettint photo details from api

--- Rocketseat-2025/05-gallery-plus/src/pages/page-photo-details.tsx
+++ Rocketseat-2025/05-gallery-plus/src/pages/page-photo-details.tsx
@@ -1,37 +1,30 @@
 import { useParams } from "react-router";
 import Text from "../components/text";
 import Container from "../components/container";
-import type { Photo } from "../contexts/photos/models/photo";
 import Skeleton from "../components/skeleton";
 import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 import ImagePreview from "../components/image-preview";
 import Button from "../components/button";
 import AlbumsListSelectable from "../contexts/albums/components/albums-list-selectable";
 import useAlbums from "../contexts/albums/hooks/user-albums";
+import usePhoto from "../contexts/photos/hooks/use-photo";
+import type { Photo } from "../contexts/photos/models/photo";
 
 export default function PagePhotoDetails() {
   const { id } = useParams();
+  const { photo, isLoadingPhoto } = usePhoto(id);
   const { albums, isLoadingAlbums } = useAlbums();
 
-  const isLoadingPhoto = false;
-  const photo = {
-    id: "1",
-    title: "Sample Photo",
-    imageId: "portrait-tower.png",
-    albums: [
-      { id: "1", title: "Sample Album" },
-      { id: "2", title: "Sample Album 2" },
-      { id: "3", title: "Sample Album 3" },
-      { id: "4", title: "Sample Album 4" },
-    ],
-  } as Photo;
+  if (!isLoadingPhoto && !photo) {
+    return <div>Photo not found</div>;
+  }
 
   return (
     <Container>
       <header className="flex items-center justify-between gap-8 mb-8">
         {!isLoadingPhoto ? (
           <Text as="h2" variant="heading-large">
-            {photo.title}
+            {photo?.title}
           </Text>
         ) : (
           <Skeleton className="w-48 h-8" />
@@ -44,7 +37,7 @@ export default function PagePhotoDetails() {
         <div className="space-y-3">
           {!isLoadingPhoto ? (
             <ImagePreview
-              src={`/images/${photo?.imageId}`}
+              src={`${import.meta.env.VITE_IMAGES_URL}/${photo?.imageId}`}
               title={photo?.title}
               imageClassName="h-[21rem]"
             />
@@ -64,7 +57,7 @@ export default function PagePhotoDetails() {
           </Text>
 
           <AlbumsListSelectable
-            photo={photo}
+            photo={photo as Photo}
             loading={isLoadingAlbums}
             albums={albums}
           />
